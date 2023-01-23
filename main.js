cfd502d7aa1ae561281a58993cffe42d28947576
Your Name <you@example.com>
2023-01-23 15:25:53 +0100

    fin du site v2

--- main.js
+++ main.js
@@ -1,11 +1,45 @@
-let btBurger =document.getElementById('burger');
-let mainMenu = document.getElementById ('main-menu');
-let btClose = document.getElementById ('close');
-btBurger.addEventListener('click', function(){
+let btBurger = document.getElementById('burger');
+let mainMenu = document.getElementById('main-menu');
+let btClose = document.getElementById('close');
+btBurger.addEventListener('click', function () {
    mainMenu.classList.toggle('active');
 
 })
-btClose.addEventListener('click', function(){
+btClose.addEventListener('click', function () {
    mainMenu.classList.toggle('active');
 
 })
+let rightArrow = document.getElementById('rightArrow');
+let leftArrow = document.getElementById('leftArrow');
+let listSlide = document.getElementById('list-slide');
+let windowSlider = document.getElementById('window-slider').offsetWidth;
+let slide = document.getElementsByClassName('slide');
+let nbSlide = slide.length;
+let mLeft = 0;
+
+
+rightArrow.addEventListener('click', function () {
+   if (mLeft - windowSlider == windowSlider*-6) {
+        
+      this.click.stopPropagation()
+  }
+  else {
+      mLeft -= windowSlider;
+      listSlide.style.marginLeft = mLeft + 'px';
+  }
+});
+
+
+
+leftArrow.addEventListener('click', function () {
+   if (mLeft + windowSlider == windowSlider) {
+        
+      this.click.stopPropagation()
+  }
+  else {
+      mLeft += windowSlider;
+      listSlide.style.marginLeft = mLeft + 'px';
+  }
+
+});
+
