1c4afbadb413c68bb279b0eec8d196cad54f813e
Your Name <you@example.com>
2023-01-24 15:03:53 +0100

    swipe js

--- main.js
+++ main.js
@@ -19,27 +19,81 @@ let mLeft = 0;
 
 
 rightArrow.addEventListener('click', function () {
-   if (mLeft - windowSlider == windowSlider*-6) {
-        
+   if (mLeft - windowSlider == windowSlider * -6) {
+
       this.click.stopPropagation()
-  }
-  else {
+   }
+   else {
       mLeft -= windowSlider;
       listSlide.style.marginLeft = mLeft + 'px';
-  }
+   }
 });
 
 
 
 leftArrow.addEventListener('click', function () {
    if (mLeft + windowSlider == windowSlider) {
-        
+
       this.click.stopPropagation()
-  }
-  else {
+   }
+   else {
       mLeft += windowSlider;
       listSlide.style.marginLeft = mLeft + 'px';
-  }
+   }
 
 });
+let currentSlide = 0;
+
+
+function nextSlide() {
+   slide[currentSlide].style.display = "none";
+   currentSlide = (currentSlide + 1) % slide.length;
+   slide[currentSlide].style.display = "block";
+}
+
+function prevSlide() {
+   slide[currentSlide].style.display = "none";
+   currentSlide = (currentSlide - 1 + slide.length) % slide.length;
+   slide[currentSlide].style.display = "block";
+}
+
+
+listSlide.addEventListener("touchstart", handleTouchStart, false);
+listSlide.addEventListener("touchmove", handleTouchMove, false);
+
+
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(evt) {
+   xDown = evt.touches[0].clientX;
+   yDown = evt.touches[0].clientY;
+}
+
+function handleTouchMove(evt) {
+   if (!xDown || !yDown) {
+      return;
+   }
+
+   let xUp = evt.touches[0].clientX;
+   let yUp = evt.touches[0].clientY;
+
+   let xDiff = xDown - xUp;
+   let yDiff = yDown - yUp;
+
+
+   if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0) {
+
+         nextSlide();
+      } else {
+
+         prevSlide();
+      }
+   }
+
+   xDown = null;
+   yDown = null;
+}
+
 
